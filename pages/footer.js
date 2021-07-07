978e9533725aee6ec29b7884db2713cc066c14ae
toshifelder <toshifelder@gmail.com>
2021-07-07 23:26:05 +0900

    re first commit

--- pages/footer.js
+++ pages/footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <div className={styles.footer_Tracks}>Tracks<img src={`${process.env.PUBLIC_URL}/instagram.svg`} /></div>
+        <div className={styles.footer_Tracks}>Tracks</div>
         <div className={styles.copyRight}>Copyright @ by Tracks Magazine All rights reserved.</div>
       </footer>
     );
